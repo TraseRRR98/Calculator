dbf73b01c1ba8cd066184508b98988840ed81e12
Sergey Tikhonov <166549676+TraseRRR98@users.noreply.github.com>
2024-06-04 21:15:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,13 @@ function clearDisplay() {
 
 function calculate() {
 	try {
+		if (display.value.includes('/0')) {
+            throw new Error("Division by zero");
+        }
 		display.value = eval(display.value);
+		if (!isFinite(display.value)) {
+            throw new Error("Division by zero");
+        }
 	}
 	catch (error) {
 		display.value = "Error!";
